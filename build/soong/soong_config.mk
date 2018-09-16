d70fe675d40552b8ebec8eb472876d3cec074404
D. Andrei Măceș <andrei@unlegacy-android.org>
2018-09-16 12:44:50 +0300

    soong_config: Add BOARD_CANT_REALLOCATE_OMX_BUFFERS
    * Soong product variable 'Cant_reallocate_omx_buffers'
    
    Change-Id: I47fbc0b1890e43bd8861f21df0dca22d52a9eed2
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,4 +3,5 @@ unlegacy_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '    "Needs_platform_textrels": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXTRELS)),true,false),'; \
+	echo '    "Cant_reallocate_omx_buffers": $(if $(filter true,$(BOARD_CANT_REALLOCATE_OMX_BUFFERS)),true,false),'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
