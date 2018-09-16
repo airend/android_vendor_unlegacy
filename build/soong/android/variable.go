c59e19dc718d0f1ad155b29d812fbc5aab8429b0
D. Andrei Măceș <andrei@unlegacy-android.org>
2018-09-16 12:44:50 +0300

    soong_config: Add BOARD_CANT_REALLOCATE_OMX_BUFFERS
    * Soong product variable 'Cant_reallocate_omx_buffers'
    
    Change-Id: I47fbc0b1890e43bd8861f21df0dca22d52a9eed2
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,8 +3,12 @@ type Product_variables struct {
 	Needs_platform_textrels struct {
 		Cppflags []string
 	}
+	Cant_reallocate_omx_buffers struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
 	Needs_platform_textrels  *bool `json:",omitempty"`
+	Cant_reallocate_omx_buffers  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,1 +1,2 @@
 $(call add_json_bool, Needs_platform_textrels, $(filter true,$(TARGET_NEEDS_PLATFORM_TEXTRELS)))
+$(call add_json_bool, Cant_reallocate_omx_buffers, $(filter true,$(BOARD_CANT_REALLOCATE_OMX_BUFFERS)))
